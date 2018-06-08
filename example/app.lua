aa3b6b5ee6a02e4fc19d1e2bfc636785dab77abe
LunaCI <lunaci@luadist.org>
2018-06-08 02:55:54 +0000

    Update to version 0.1.0-0

---
+++ example/app.lua
@@ -0,0 +1,14 @@
+-- setup path to find the project source files of Pegasus
+package.path = "./src/?.lua;./src/?/init.lua;"..package.path
+
+local Pegasus = require 'pegasus'
+local Compress = require 'pegasus.compress'
+
+local server = Pegasus:new({
+  port='9090',
+  location='/example/root/',
+  plugins = { Compress:new() }
+})
+
+server:start(function(req, rep)
+end)
